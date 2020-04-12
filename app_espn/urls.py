ac9d3470200593f01225950b5c108a97748c47df
adul11s <romli@alterra.id>
2020-04-12 16:58:32 +0700

    fix html

--- app_espn/urls.py
+++ app_espn/urls.py
@@ -5,6 +5,13 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('artikel/', views.artikel, name='artikel'),
-    path('signup/', views.signup, name="signup"),
+    path('artikel', views.artikel, name='artikel'),
+    path('football', views.football, name="football"),
+    path('nba', views.nba, name="nba"),
+    path('nfl', views.nfl, name="nfl"),
+    path('cricket', views.cricket, name="cricket"),
+    path('rugby', views.rugby, name="rugby"),
+    path('golf', views.golf, name="golf")
+    
+
 ]
