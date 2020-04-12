06cf973ec4116ab984714fd4edf0c45b00c3c08c
roslianiwp <rosliani@alterra.id>
2020-04-12 18:44:39 +0700

    update terbaru dgn sistem

--- app_espn/urls.py
+++ app_espn/urls.py
@@ -1,17 +1,23 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf.urls import url
-# from mysite.core import views as core_views
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('artikel', views.artikel, name='artikel'),
+    path('artikel/', views.artikel, name='artikel'),
+    path('signup/', views.signup, name="signup"),
+    path('search/', views.search, name="search"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('artikel/<int:artikel_id>/', views.artikel_detail, name='artikel_detail'),
+    path('artikel/clap/<int:artikel_id>/',views.artikel_detail_add_like),
     path('football', views.football, name="football"),
     path('nba', views.nba, name="nba"),
     path('nfl', views.nfl, name="nfl"),
     path('cricket', views.cricket, name="cricket"),
     path('rugby', views.rugby, name="rugby"),
     path('golf', views.golf, name="golf")
-    
-
 ]
+
+
